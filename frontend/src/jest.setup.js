77044c9ef41214281b8c5e99800ed0983dbb7552
Uzuri CI <backend-ci@example.com>
2025-10-02 11:16:33 +0300

    test: extend jest.setup to patch QueryClient from react-query and query-core

--- frontend/src/jest.setup.js
+++ frontend/src/jest.setup.js
@@ -1,9 +1,24 @@
 // Global Jest setup: ensure QueryClient has defaultMutationOptions used by useMutation
 try {
-  const { QueryClient } = require('@tanstack/react-query')
-  if (QueryClient && typeof QueryClient.prototype.defaultMutationOptions !== 'function') {
-    // eslint-disable-next-line no-extend-native
-    QueryClient.prototype.defaultMutationOptions = function (opts) { return opts || {} }
+  // Patch QueryClient from react-query if present
+  try {
+    const rq = require('@tanstack/react-query')
+    if (rq && rq.QueryClient && typeof rq.QueryClient.prototype.defaultMutationOptions !== 'function') {
+      // eslint-disable-next-line no-extend-native
+      rq.QueryClient.prototype.defaultMutationOptions = function (opts) { return opts || {} }
+    }
+  } catch (e) {
+    // ignore
+  }
+  // Patch QueryClient from query-core if present
+  try {
+    const qc = require('@tanstack/query-core')
+    if (qc && qc.QueryClient && typeof qc.QueryClient.prototype.defaultMutationOptions !== 'function') {
+      // eslint-disable-next-line no-extend-native
+      qc.QueryClient.prototype.defaultMutationOptions = function (opts) { return opts || {} }
+    }
+  } catch (e) {
+    // ignore
   }
 } catch (e) {
   // ignore if package not present
